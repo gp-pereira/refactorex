093781084b6722a97d944f77740b5e0981cf2483
gp-pereira <pereira7346@gmail.com>
2024-04-09 21:33:06 -0300

    feat: split changed text into diffs

---
+++ lib/refactorex/diff.ex
@@ -0,0 +1,84 @@
+defmodule Refactorex.Diff do
+  def find_diffs(original, refactored) do
+    original = original |> String.trim() |> String.split("\n")
+    refactored = refactored |> String.trim() |> String.split("\n")
+
+    original
+    |> List.myers_difference(refactored)
+    |> accumulate_diffs([], 1)
+  end
+
+  defp accumulate_diffs([], diffs, _), do: diffs
+
+  defp accumulate_diffs([{:eq, lines} | rest], diffs, line),
+    do: accumulate_diffs(rest, diffs, length(lines) + line)
+
+  defp accumulate_diffs([{:del, del} | [{:ins, ins} | rest]], diffs, line) do
+    accumulate_diffs(
+      rest,
+      [
+        %{
+          text: "#{Enum.join(ins, "\n")}",
+          range: %{
+            start: %{
+              line: line,
+              character: 0
+            },
+            end: %{
+              line: line + length(del) - 2,
+              character: del |> List.last() |> String.length()
+            }
+          }
+        }
+        | diffs
+      ],
+      line + length(del)
+    )
+  end
+
+  defp accumulate_diffs([{:ins, ins} | rest], diffs, line) do
+    accumulate_diffs(
+      rest,
+      [
+        %{
+          text: (ins ++ [""]) |> Enum.join("\n"),
+          range: %{
+            start: %{
+              line: line,
+              character: 0
+            },
+            end: %{
+              line: line,
+              character: 0
+            }
+          }
+        }
+        | diffs
+      ],
+      line + 1
+    )
+  end
+
+  defp accumulate_diffs([{:del, del} | rest], diffs, line) do
+    accumulate_diffs(
+      rest,
+      [
+        %{
+          text: "",
+          range: %{
+            start: %{
+              line: line,
+              character: 0
+            },
+            end: %{
+              line: line + length(del) - 1,
+              character: del |> List.last() |> String.length()
+            }
+          }
+        }
+        | diffs
+      ],
+      line + length(del)
+    )
+  end
+end
---
+++ test/diff_test.exs
@@ -0,0 +1,107 @@
+defmodule Refactorex.DiffTest do
+  use ExUnit.Case
+
+  alias Refactorex.Diff
+
+  test "grabs ranges of modified text" do
+    assert Diff.find_diffs(
+             """
+             defmodule Foo do
+              def bar(arg) do
+                arg
+              end
+             end
+             """,
+             """
+             defmodule Foo do
+              def bar(arg), do: arg
+             end
+             """
+           ) == [
+             %{
+               text: " def bar(arg), do: arg",
+               range: %{
+                 start: %{line: 2, character: 0},
+                 end: %{line: 3, character: 4}
+               }
+             }
+           ]
+  end
+
+  test "grabs ranges of inserted text" do
+    assert Diff.find_diffs(
+             """
+             defmodule Foo do
+              def bar do
+              end
+             end
+             """,
+             """
+             defmodule Foo do
+              def bar do
+              end
+
+              def baz(arg) do
+                arg + 10
+              end
+             end
+             """
+           ) == [
+             %{
+               text: "\n def baz(arg) do\n   arg + 10\n end\n",
+               range: %{
+                 start: %{line: 4, character: 0},
+                 end: %{line: 4, character: 0}
+               }
+             }
+           ]
+  end
+
+  test "grabs ranges of deleted text" do
+    assert Diff.find_diffs(
+             """
+             defmodule Foo do
+              def bar do
+              end
+
+              def baz(arg) do
+                arg + 10
+              end
+             end
+             """,
+             """
+             defmodule Foo do
+              def bar do
+              end
+             end
+             """
+           ) == [
+             %{
+               text: "",
+               range: %{
+                 start: %{line: 4, character: 0},
+                 end: %{line: 7, character: 4}
+               }
+             }
+           ]
+  end
+
+  test "grabs no ranges form same text" do
+    assert Diff.find_diffs(
+             """
+             defmodule Foo do
+              def bar(arg) do
+                arg
+              end
+             end
+             """,
+             """
+             defmodule Foo do
+              def bar(arg) do
+                arg
+              end
+             end
+             """
+           ) == []
+  end
+end
