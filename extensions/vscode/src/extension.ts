2e107be06161f8a8f1196c2b96a2b3d35c73eb81
gp-pereira <pereira7346@gmail.com>
2024-11-23 17:40:27 -0300

    refactor: improved client start up

--- extensions/vscode/src/extension.ts
+++ extensions/vscode/src/extension.ts
@@ -7,27 +7,24 @@ import * as net from "net";
 let client: LanguageClient;
 let server: ChildProcess;
 
-export function activate(context: ExtensionContext) {
+export async function activate(context: ExtensionContext) {
+	const serverPath = context.asAbsolutePath("refactorex");
+
 	client = new LanguageClient(
 		"refactorex",
 		"Refactorex",
 		async () => {
-			const relativePath = path.join("refactorex", "bin", "start");
-			const command = context.asAbsolutePath(relativePath);
+			await ensureServerCompiled(serverPath);
 
 			const port = await findAvailablePort();
+			server = startServer(serverPath, port);
 
-			server = exec(`MIX_ENV=prod ${command} --port ${port}`, (error) => {
-				if (error) client.info(`Server not started: ${error}`);
-			});
-
-			// giving the server some time to compile and start
-			await new Promise((r) => setTimeout(r, 15000));
+			// giving the server some time to start
+			await new Promise((r) => setTimeout(r, 1000));
 
 			client.info(`Server started on port ${port}`);
 
 			const socket = await connect(port);
-
 			return { writer: socket, reader: socket };
 		},
 		{
@@ -39,7 +36,7 @@ export function activate(context: ExtensionContext) {
 	);
 
 	client.start();
-	client.info("Client started and waiting server");
+	client.info("Client is waiting server");
 }
 
 export function deactivate(): Thenable<void> | undefined {
@@ -48,6 +45,44 @@ export function deactivate(): Thenable<void> | undefined {
 	if (!client) return;
 
 	return client.stop();
+}
+
+function ensureServerCompiled(serverPath: string): Promise<void> {
+	const compilationPath = path.join(serverPath, "_build", "prod");
+	const compileCommand = `cd ${serverPath} && mix deps.get && mix compile`;
+
+	const command =
+		process.platform === "win32"
+			? `if not exist ${compilationPath} (set MIX_ENV=prod && ${compileCommand})`
+			: `[ ! -d "${compilationPath}" ] && (export MIX_ENV=prod && ${compileCommand}) || true`;
+
+	return new Promise((resolve, reject) => {
+		exec(command, (error, stdout, stderr) => {
+			if (!error && !stderr) {
+				client.info(stdout);
+				return resolve();
+			}
+
+			const msg = `Compilation error: ${error || stderr}`;
+			client.error(msg);
+			reject(new Error(msg));
+		});
+	});
+}
+
+function startServer(serverPath: string, port: number): ChildProcess {
+	return exec(
+		[
+			`cd ${serverPath} &&`,
+			`elixir --sname undefined -S`,
+			`mix run --no-halt -e`,
+			`"Application.ensure_all_started(:refactorex)"`,
+			`-- --port ${port}`,
+		].join(" "),
+		(error, stdout, stderr) => {
+			if (error || stderr) client.error(`Server not started\n ${error}`);
+		}
+	);
 }
 
 function findAvailablePort(): Promise<number> {
