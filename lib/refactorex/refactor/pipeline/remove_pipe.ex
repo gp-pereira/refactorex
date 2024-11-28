100456f6a395bb0918083a1ad0d5d81d4b4effb0
gp-pereira <pereira7346@gmail.com>
2024-11-27 23:35:38 -0300

    refactor: improved remove pipe to expected behaviour

--- lib/refactorex/refactor/pipeline/remove_pipe.ex
+++ lib/refactorex/refactor/pipeline/remove_pipe.ex
@@ -4,15 +4,15 @@ defmodule Refactorex.Refactor.Pipeline.RemovePipe do
     kind: "refactor.rewrite",
     works_on: :line
 
-  def can_refactor?(%{node: {:|>, _, _} = node}, line),
-    do: AST.starts_at?(node, line)
+  def can_refactor?(%{node: {:|>, meta, _}}, line),
+    do: meta[:line] == line
 
   def can_refactor?(_, _), do: false
 
   def refactor(zipper, _) do
     zipper
     |> Z.update(fn {:|>, _, [arg, {id, meta, rest}]} ->
-      {id, meta, [arg | rest]}
+      {id, meta, [arg |> IO.inspect() | rest]}
     end)
   end
 end
--- test/refactor/pipeline/remove_pipe_test.exs
+++ test/refactor/pipeline/remove_pipe_test.exs
@@ -16,6 +16,66 @@ defmodule Refactorex.Refactor.Pipeline.RemovePipeTest do
     )
   end
 
+  test "removes pipe operator from start of pipeline" do
+    assert_refactored(
+      RemovePipe,
+      """
+      arg1
+      #  v
+      |> foo(arg2)
+      |> bar()
+      """,
+      """
+      foo(
+        arg1,
+        arg2
+      )
+      |> bar()
+      """
+    )
+  end
+
+  test "removes pipe operator from middle of pipeline" do
+    assert_refactored(
+      RemovePipe,
+      """
+      arg1
+      |> bar()
+      #  v
+      |> foo(arg2)
+      |> bar()
+      """,
+      """
+      foo(
+        arg1
+        |> bar(),
+        arg2
+      )
+      |> bar()
+      """
+    )
+  end
+
+  test "removes pipe operator from end of pipeline" do
+    assert_refactored(
+      RemovePipe,
+      """
+      arg1
+      |> bar()
+      |> foo(arg2)
+      #  v
+      |> bar()
+      """,
+      """
+      bar(
+        arg1
+        |> bar()
+        |> foo(arg2)
+      )
+      """
+    )
+  end
+
   test "ignores range without pipes" do
     assert_not_refactored(
       RemovePipe,
