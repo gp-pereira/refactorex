967d25f4e4b967d91141b79cc57ec8fa55485921
gp-pereira <pereira7346@gmail.com>
2025-01-19 15:17:05 -0300

    feat: dataflow analysis algorithm

---
+++ lib/refactorex/dataflow.ex
@@ -0,0 +1,177 @@
+defmodule Refactorex.Dataflow do
+  alias Sourceror.Zipper, as: Z
+
+  defguard is_variable(node)
+           when is_tuple(node) and
+                  tuple_size(node) == 3 and
+                  is_atom(elem(node, 0)) and
+                  is_nil(elem(node, 2))
+
+  defstruct commands: [],
+            variables: []
+
+  def analyze(node) do
+    %__MODULE__{}
+    |> recursive_analyze(node)
+    |> Map.get(:variables)
+    |> Enum.map(&{&1.declaration, &1.usages})
+    |> Map.new()
+  end
+
+  defp recursive_analyze(dataflow, zipper) do
+    case zipper do
+      {id, _, [{_, _, header}, body]} when id in ~w(def defp)a ->
+        analyze_sealed_scope(dataflow, header, body)
+
+      {id, _, [{:when, _, [header, body]}]} when id in ~w(defguard defguardp)a ->
+        analyze_sealed_scope(dataflow, header, body)
+
+      {id, _, [condition, clauses]} when id in ~w(if unless)a ->
+        %__MODULE__{}
+        |> recursive_analyze(condition)
+        |> then(fn if_dataflow ->
+          Enum.reduce(clauses, if_dataflow, &analyze_scope(&2, &1))
+        end)
+        |> close_scope(dataflow)
+
+      {:cond, _, [[{_, clauses}]]} ->
+        Enum.reduce(clauses, dataflow, fn
+          {:->, _, clause}, dataflow -> analyze_scope(dataflow, clause)
+        end)
+
+      {:try, _, [[body | catches]]} ->
+        %__MODULE__{}
+        |> analyze_scope(body)
+        |> recursive_analyze(catches)
+        |> close_scope(dataflow)
+
+      {:case, _, expression_and_clauses} ->
+        analyze_compound_scope(dataflow, expression_and_clauses, [])
+
+      {:with, _, children} ->
+        {
+          statements,
+          [[body | catches]]
+        } = Enum.split_while(children, &match?({:<-, _, _}, &1))
+
+        dataflow
+        |> analyze_compound_scope(statements, body)
+        |> recursive_analyze(catches)
+
+      {:for, _, children} ->
+        {statements, body} = Enum.split_while(children, &match?({_, _, _}, &1))
+        analyze_compound_scope(dataflow, statements, body)
+
+      {{:__block__, _, [:do]}, block} ->
+        analyze_scope(dataflow, block)
+
+      {:test, _, [_, {:%{}, _, setup}, scope]} ->
+        analyze_sealed_scope(dataflow, setup, scope)
+
+      {:test, _, scope} ->
+        analyze_sealed_scope(dataflow, scope)
+
+      {:->, _, [left, right]} ->
+        analyze_scope(dataflow, left, right)
+
+      {id, _, [left, right]} when id in ~w(= <-)a ->
+        dataflow
+        |> recursive_analyze(right)
+        |> add_commands(gen_commands(left))
+
+      {:@, _, [node]} when is_variable(node) ->
+        dataflow
+
+      node when is_variable(node) ->
+        add_commands(dataflow, [{:use, node}])
+
+      node ->
+        if children = Z.children(node),
+          do: Enum.reduce(children, dataflow, &recursive_analyze(&2, &1)),
+          else: dataflow
+    end
+  end
+
+  defp analyze_compound_scope(dataflow, before_statements, scope) do
+    before_statements
+    |> Enum.reduce(%__MODULE__{}, &recursive_analyze(&2, &1))
+    |> analyze_scope(scope)
+    |> close_scope(dataflow)
+  end
+
+  defp analyze_sealed_scope(dataflow, maybe_declarations \\ [], scope) do
+    dataflow
+    |> analyze_scope(maybe_declarations, scope)
+    # don't let commands leak to outer scope
+    |> Map.put(:commands, dataflow.commands)
+  end
+
+  defp analyze_scope(dataflow, maybe_declarations \\ [], scope) do
+    %__MODULE__{}
+    |> add_commands(gen_commands(maybe_declarations))
+    |> recursive_analyze(scope)
+    |> close_scope(dataflow)
+  end
+
+  defp close_scope(scope_dataflow, dataflow) do
+    %{commands: scoped_commands, variables: inner_scoped_variables} = scope_dataflow
+
+    # process all remaining commands inside the current scope into a new dataflow
+    %{commands: unused_commands, variables: scoped_variables} =
+      scoped_commands
+      |> Enum.reverse()
+      |> Enum.reduce(%__MODULE__{}, &process_command/2)
+
+    %{
+      dataflow
+      | # unused commands (usages) are passed to the outer scope
+        commands: unused_commands ++ dataflow.commands,
+        variables: scoped_variables ++ inner_scoped_variables ++ dataflow.variables
+    }
+  end
+
+  defp process_command(command, %{variables: variables} = scoped_dataflow) do
+    case command do
+      {:gen, {name, _, _} = variable} ->
+        variable = %{name: name, declaration: variable, usages: []}
+
+        %{scoped_dataflow | variables: [variable | variables]}
+
+      {:use, {name, _, _} = variable} ->
+        if i = Enum.find_index(variables, &(&1.name == name)) do
+          variables = update_in(variables, [Access.at(i), :usages], &[variable | &1])
+
+          %{scoped_dataflow | variables: variables}
+        else
+          add_commands(scoped_dataflow, [command])
+        end
+    end
+  end
+
+  defp gen_commands(node) do
+    node
+    |> Z.zip()
+    |> Z.traverse_while([], fn
+      %{node: {:@, _, [node]}} = zipper, commands when is_variable(node) ->
+        {:skip, zipper, commands}
+
+      %{node: {:^, _, [node]}} = zipper, commands when is_variable(node) ->
+        {:skip, zipper, [{:use, node} | commands]}
+
+      %{node: {name, _, _} = node} = zipper, commands when is_variable(node) ->
+        command =
+          if Enum.any?(commands, fn {_, {n, _, _}} -> n == name end),
+            do: {:use, node},
+            else: {:gen, node}
+
+        {:cont, zipper, [command | commands]}
+
+      zipper, commands ->
+        {:cont, zipper, commands}
+    end)
+    |> elem(1)
+  end
+
+  defp add_commands(dataflow, commands),
+    do: %{dataflow | commands: commands ++ dataflow.commands}
+end
---
+++ test/dataflow_test.exs
@@ -0,0 +1,494 @@
+defmodule Refactorex.DataflowTest do
+  use ExUnit.Case
+
+  alias Refactorex.Dataflow
+
+  defmacrop variable_at(name, line, column) do
+    quote do
+      {unquote(name),
+       [
+         trailing_comments: [],
+         leading_comments: [],
+         line: unquote(line),
+         column: unquote(column)
+       ], nil}
+    end
+  end
+
+  test "lists all variables inside simple function with constants" do
+    assert %{
+             variable_at(:arg1, 1, 9) => [
+               variable_at(:arg1, 2, 10)
+             ],
+             variable_at(:arg2, 1, 15) => [
+               variable_at(:arg2, 2, 3)
+             ]
+           } =
+             Dataflow.analyze(
+               """
+               def bar(arg1, arg2 \\\\ @arg2) do
+                 arg2 + arg1 + @arg1
+               end
+               """
+               |> Sourceror.parse_string!()
+             )
+  end
+
+  test "lists all variables inside same arg twice function" do
+    assert %{
+             variable_at(:arg, 1, 17) => [
+               variable_at(:arg, 1, 33),
+               variable_at(:arg, 1, 23)
+             ]
+           } =
+             Dataflow.analyze(
+               """
+               defp bar(%{arg: arg}, arg), do: arg
+               """
+               |> Sourceror.parse_string!()
+             )
+  end
+
+  test "lists all variables inside function with guard" do
+    assert %{
+             variable_at(:arg, 1, 10) => [
+               variable_at(:arg, 1, 20)
+             ]
+           } =
+             Dataflow.analyze(
+               """
+               defp bar(arg) when arg > 10 do
+               end
+               """
+               |> Sourceror.parse_string!()
+             )
+  end
+
+  test "lists all variables inside anonymous function" do
+    assert %{
+             variable_at(:list, 1, 10) => [
+               variable_at(:list, 2, 12)
+             ],
+             variable_at(:arg1, 1, 16) => [
+               variable_at(:arg1, 10, 3),
+               {:arg1,
+                [
+                  trailing_comments: [],
+                  leading_comments: [],
+                  # bummer
+                  end_of_expression: [newlines: 2, line: 4, column: 12],
+                  line: 4,
+                  column: 8
+                ], nil},
+               variable_at(:arg1, 3, 6)
+             ],
+             variable_at(:arg1, 6, 13) => [
+               variable_at(:arg1, 7, 7),
+               variable_at(:arg1, 6, 24)
+             ]
+           } =
+             Dataflow.analyze(
+               """
+               defp bar(list, arg1) do
+                 Enum.map(list, fn
+                   ^arg1 ->
+                      arg1
+
+                   %{arg1: arg1} when arg1 > 10 ->
+                     arg1
+                 end)
+
+                 arg1
+               end
+               """
+               |> Sourceror.parse_string!()
+             )
+  end
+
+  test "lists all variables inside guard" do
+    assert %{
+             variable_at(:arg, 1, 16) => [
+               variable_at(:arg, 1, 26)
+             ]
+           } =
+             Dataflow.analyze(
+               """
+               defguard guard(arg) when arg < 10
+               """
+               |> Sourceror.parse_string!()
+             )
+  end
+
+  test "lists all variables after a reassignment" do
+    assert %{
+             variable_at(:arg, 1, 9) => [
+               variable_at(:arg, 2, 9)
+             ],
+             variable_at(:arg, 2, 3) => [
+               variable_at(:arg, 3, 3)
+             ]
+           } =
+             Dataflow.analyze(
+               """
+               def bar(arg) do
+                 arg = arg + 10
+                 arg * 20
+               end
+               """
+               |> Sourceror.parse_string!()
+             )
+  end
+
+  test "lists all variables inside CASE statement" do
+    assert %{
+             variable_at(:arg1, 1, 9) => [
+               variable_at(:arg1, 11, 10),
+               variable_at(:arg1, 2, 15)
+             ],
+             variable_at(:arg2, 1, 15) => [
+               variable_at(:arg2, 11, 3),
+               variable_at(:arg2, 2, 22)
+             ],
+             variable_at(:arg2, 2, 8) => [
+               variable_at(:arg2, 4, 14)
+             ],
+             variable_at(:arg2, 4, 7) => [
+               variable_at(:arg2, 5, 7)
+             ],
+             variable_at(:arg1, 7, 5) => [
+               variable_at(:arg1, 8, 7),
+               variable_at(:arg1, 7, 15)
+             ]
+           } =
+             Dataflow.analyze(
+               """
+               def bar(arg1, arg2) do
+                 case arg2 = arg1 + arg2 do
+                   20 ->
+                     arg2 = arg2 + 5
+                     arg2 + 40
+
+                   arg1 when arg1 < 1 ->
+                     arg1 + 12
+                 end
+
+                 arg2 + arg1
+               end
+               """
+               |> Sourceror.parse_string!()
+             )
+  end
+
+  test "lists all variables inside COND statement" do
+    assert %{
+             variable_at(:arg, 1, 9) => [
+               variable_at(:arg, 10, 3),
+               variable_at(:arg, 6, 11),
+               variable_at(:arg, 4, 7),
+               variable_at(:arg, 3, 5)
+             ],
+             variable_at(:arg, 6, 5) => [
+               variable_at(:arg, 7, 7),
+               variable_at(:arg, 6, 25)
+             ]
+           } =
+             Dataflow.analyze(
+               """
+               def bar(arg) do
+                 cond do
+                   arg == 15 ->
+                     arg * 15
+
+                   arg = arg + 10 when arg > 10 ->
+                     arg * 10
+                 end
+
+                 arg
+               end
+               """
+               |> Sourceror.parse_string!()
+             )
+  end
+
+  test "lists all variables inside WITH statement" do
+    assert %{
+             variable_at(:arg1, 1, 9) => [
+               variable_at(:arg1, 13, 3),
+               variable_at(:arg1, 9, 10),
+               variable_at(:arg1, 3, 16),
+               variable_at(:arg1, 2, 16)
+             ],
+             variable_at(:arg2, 2, 8) => [
+               variable_at(:arg2, 4, 23),
+               variable_at(:arg2, 3, 25)
+             ],
+             variable_at(:arg1, 4, 8) => [
+               variable_at(:arg1, 6, 19),
+               variable_at(:arg1, 5, 31),
+               variable_at(:arg1, 4, 15)
+             ],
+             variable_at(:arg2, 5, 8) => [
+               variable_at(:arg2, 6, 12),
+               variable_at(:arg2, 5, 18)
+             ],
+             variable_at(:arg1, 6, 5) => [
+               variable_at(:arg1, 7, 5)
+             ],
+             variable_at(:e, 9, 5) => [
+               variable_at(:e, 9, 17)
+             ],
+             variable_at(:arg1, 10, 5) => [
+               variable_at(:arg1, 10, 13)
+             ]
+           } =
+             Dataflow.analyze(
+               """
+               def bar(arg1) do
+                 with arg2 <- arg1,
+                      %{arg: ^arg1} <- arg2,
+                      arg1 = arg1 <- arg2,
+                      arg2 when arg2 < 20 <- arg1 do
+                   arg1 = arg2 + arg1
+                   arg1 * 10
+                 else
+                   e -> arg1 + e
+                   arg1 -> arg1
+                 end
+
+                 arg1
+               end
+               """
+               |> Sourceror.parse_string!()
+             )
+  end
+
+  test "lists all variables inside TRY statement" do
+    assert %{
+             variable_at(:arg, 1, 9) => [
+               variable_at(:arg, 14, 3),
+               variable_at(:arg, 10, 11),
+               variable_at(:arg, 8, 14),
+               variable_at(:arg, 3, 11)
+             ],
+             variable_at(:arg, 3, 5) => [
+               variable_at(:arg, 4, 5)
+             ],
+             variable_at(:arg, 6, 5) => [
+               variable_at(:arg, 6, 12)
+             ],
+             variable_at(:e, 8, 5) => [
+               variable_at(:e, 8, 10)
+             ],
+             variable_at(:arg, 10, 5) => [
+               variable_at(:arg, 11, 5)
+             ]
+           } =
+             Dataflow.analyze(
+               """
+               def bar(arg) do
+                 try do
+                   arg = arg * 15
+                   arg + 10
+                 rescue
+                   arg -> arg
+                 catch
+                   e -> e + arg
+                 after
+                   arg = arg + 10
+                   arg
+                 end
+
+                 arg
+               end
+               """
+               |> Sourceror.parse_string!()
+             )
+  end
+
+  test "lists all variables inside IF statement" do
+    assert %{
+             variable_at(:arg, 1, 9) => [
+               variable_at(:arg, 10, 3),
+               variable_at(:arg, 2, 12)
+             ],
+             variable_at(:arg, 2, 6) => [
+               variable_at(:arg, 6, 11),
+               variable_at(:arg, 3, 11)
+             ],
+             variable_at(:arg, 3, 5) => [
+               variable_at(:arg, 4, 5)
+             ],
+             variable_at(:arg, 6, 5) => [
+               variable_at(:arg, 7, 5)
+             ]
+           } =
+             Dataflow.analyze(
+               """
+               def bar(arg) do
+                 if arg = arg + 10 do
+                   arg = arg * 15
+                   arg + 10
+                 else
+                   arg = arg * 20
+                   arg + 25
+                 end
+
+                 arg
+               end
+               """
+               |> Sourceror.parse_string!()
+             )
+  end
+
+  test "lists all variables inside UNLESS statement" do
+    assert %{
+             variable_at(:arg, 1, 9) => [
+               variable_at(:arg, 10, 3),
+               variable_at(:arg, 2, 16)
+             ],
+             variable_at(:arg, 2, 10) => [
+               variable_at(:arg, 6, 11),
+               variable_at(:arg, 3, 11)
+             ],
+             variable_at(:arg, 3, 5) => [
+               variable_at(:arg, 4, 5)
+             ],
+             variable_at(:arg, 6, 5) => [
+               variable_at(:arg, 7, 5)
+             ]
+           } =
+             Dataflow.analyze(
+               """
+               def bar(arg) do
+                 unless arg = arg + 10 do
+                   arg = arg * 15
+                   arg + 10
+                 else
+                   arg = arg * 20
+                   arg + 25
+                 end
+
+                 arg
+               end
+               """
+               |> Sourceror.parse_string!()
+             )
+  end
+
+  test "lists all variables inside FOR statement" do
+    assert %{
+             variable_at(:arg1, 1, 9) => [
+               variable_at(:arg1, 11, 3),
+               variable_at(:arg1, 4, 18)
+             ],
+             variable_at(:arg2, 2, 7) => [
+               variable_at(:arg2, 3, 14)
+             ],
+             variable_at(:arg2, 3, 7) => [
+               variable_at(:arg2, 7, 19),
+               variable_at(:arg2, 5, 14)
+             ],
+             variable_at(:arg1, 4, 7) => [
+               variable_at(:arg1, 7, 13),
+               variable_at(:arg1, 5, 7)
+             ],
+             variable_at(:arg1, 7, 5) => [
+               variable_at(:arg1, 8, 5)
+             ]
+           } =
+             Dataflow.analyze(
+               """
+               def bar(arg1) do
+                 for arg2 <- 1..5,
+                     arg2 = arg2 + 10,
+                     arg1 <- 0..arg1,
+                     arg1 + arg2 < 4,
+                     into: %{} do
+                   arg1 = {arg1, arg2}
+                   arg1
+                 end
+
+                 arg1
+               end
+               """
+               |> Sourceror.parse_string!()
+             )
+  end
+
+  test "lists all variables inside test macro" do
+    assert %{
+             variable_at(:arg, 2, 3) => [
+               variable_at(:arg, 15, 3)
+             ],
+             variable_at(:arg, 4, 38) => [
+               variable_at(:arg, 5, 11)
+             ],
+             variable_at(:arg, 5, 5) => [
+               variable_at(:arg, 6, 5)
+             ],
+             variable_at(:arg, 11, 5) => [
+               variable_at(:arg, 12, 5)
+             ]
+           } =
+             Dataflow.analyze(
+               """
+               defmodule Test do
+                 arg = 10
+
+                 test "test with variables", %{arg: arg} do
+                   arg = arg + 10
+                   arg
+                 end
+
+                 test "test with variables" do
+                   arg # ignored
+                   arg = arg + 20
+                   arg
+                 end
+
+                 arg
+               end
+               """
+               |> Sourceror.parse_string!()
+             )
+  end
+
+  test "lists all variables inside module" do
+    assert %{
+             variable_at(:arg, 2, 3) => [
+               variable_at(:arg, 18, 3),
+               variable_at(:arg, 5, 11)
+             ],
+             variable_at(:arg, 5, 5) => [
+               variable_at(:arg, 15, 5)
+             ],
+             variable_at(:arg, 11, 19) => [
+               variable_at(:arg, 12, 7)
+             ]
+           } =
+             Dataflow.analyze(
+               """
+               defmodule Outer do
+                 arg = 10
+
+                 defmodule Mod do
+                   arg = arg + 20
+
+                   def function() do
+                     arg # ignored
+                   end
+
+                   def function2(arg) do
+                     arg
+                   end
+
+                   arg
+                 end
+
+                 arg + 20
+               end
+               """
+               |> Sourceror.parse_string!()
+             )
+  end
+end
